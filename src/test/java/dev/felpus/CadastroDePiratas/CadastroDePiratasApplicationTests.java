0f86e2cd5004915f249d9458fc842a71a86cc193
Your Name <you@example.com>
2026-02-26 21:37:29 -0300

    first commit

---
+++ src/test/java/dev/felpus/CadastroDePiratas/CadastroDePiratasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.felpus.CadastroDePiratas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePiratasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
