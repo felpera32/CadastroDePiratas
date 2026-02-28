cd9ade17a1abcd6133e7376d4e3b89358daec5fc
Your Name <you@example.com>
2026-02-27 21:08:17 -0300

    Mudança de projeto, para cadastro de Ninjas

--- src/main/java/dev/felpus/CadastroDePiratas/Ninjas/NinjaController.java
+++ src/main/java/dev/felpus/CadastroDePiratas/Ninjas/NinjaController.java
@@ -1,4 +1,4 @@
-package dev.felpus.CadastroDePiratas;
+package dev.felpus.CadastroDePiratas.Ninjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class PirataController {
+public class NinjaController {
 
     @GetMapping("/msg")
     public String boasVindas(){
--- src/main/java/dev/felpus/CadastroDePiratas/Ninjas/NinjaModel.java
+++ src/main/java/dev/felpus/CadastroDePiratas/Ninjas/NinjaModel.java
@@ -1,30 +1,27 @@
-package dev.felpus.CadastroDePiratas;
+package dev.felpus.CadastroDePiratas.Ninjas;
 import jakarta.persistence.*;
 
 
 // Entity transforma uma classe uma entidade do DB
 @Entity
 @Table(name = "tb_cadastro")
-public class PirataModel {
+public class NinjaModel {
 
     @Id //Fala q o atributo abaixo e o ID
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private int idade;
-    private String tripulacao;
-    private Boolean isAlive;
+
     private String email;
 
-    public PirataModel() {
+    public NinjaModel() {
     }
 
-    public PirataModel(Long id, String nome, int idade, String tripulacao, Boolean isAlive, String email) {
+    public NinjaModel(Long id, String nome, int idade, String email) {
         this.id = id;
         this.nome = nome;
         this.idade = idade;
-        this.tripulacao = tripulacao;
-        this.isAlive = isAlive;
         this.email = email;
     }
 
@@ -50,22 +47,6 @@ public class PirataModel {
 
     public void setIdade(int idade) {
         this.idade = idade;
-    }
-
-    public String getTripulacao() {
-        return tripulacao;
-    }
-
-    public void setTripulacao(String tripulacao) {
-        this.tripulacao = tripulacao;
-    }
-
-    public Boolean getAlive() {
-        return isAlive;
-    }
-
-    public void setAlive(Boolean alive) {
-        isAlive = alive;
     }
 
     public String getEmail() {
